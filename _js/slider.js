22a88ff6b0a358022f4423177ffd667107eef550
Andrew MacMurray <a.macmurray@icloud.com>
2017-02-27 20:12:34 +0000

    start slider

--- _js/slider.js
+++ _js/slider.js
@@ -1,7 +1,25 @@
-$('.slider').slick({
+var slider = $('.slider')
+var sliderNav = $('.slider-nav')
+
+slider.slick({
   fade: true,
-  dots: true,
   waitForAnimate: false,
   prevArrow: '#previous',
   nextArrow: '#next'
+  // dots: true,
+  // customPaging: function (slider, i) {
+  //   const imgUrl = $(slider.$slides[i]).find('[imgdata]')[0].getAttribute('imgdata')
+  //   return `
+  //     <div class="w5 h5">
+  //       <div class="w-100 h-100 bg-center cover" style="background-image: url(${imgUrl})"
+  //     </div>
+  //   `
+  // }
+})
+
+sliderNav.slick({
+  slidesToShow: 5,
+  arrows: true,
+  fade: true,
+  asNavFor: '.slider'
 })
