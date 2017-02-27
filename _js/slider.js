3f12b74afa9b5d2eceaf3fb78460e50b7b4eea7d
Andrew MacMurray <a.macmurray@icloud.com>
2017-02-27 22:34:34 +0000

    url structure for individual project pages, hook up pandora and strange fruit

--- _js/slider.js
+++ _js/slider.js
@@ -11,7 +11,7 @@ slider.slick({
   customPaging: function (slider, i) {
     const imgUrl = $(slider.$slides[i]).find('[imgdata]')[0].getAttribute('imgdata')
     return `
-      <div class="w2 h2 w3-ns h3-ns pointer mt4">
+      <div class="w2 h2 w3-ns h3-ns pointer">
         <div class="w-100 h-100 bg-center contain" style="background-image: url(${imgUrl})"
       </div>
     `
@@ -19,5 +19,6 @@ slider.slick({
 })
 
 captions.slick({
-  fade: true
+  fade: true,
+  arrows: false
 })
